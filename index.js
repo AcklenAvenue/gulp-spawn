86a112a94246d9f10ba356a9b87b31f45b4f0049
Gazy Mahomar <gmahomarf@gmail.com>
2016-04-13 20:58:09 -0600

    Updated dependencies and added features
    - once
    - each
    - stream
    - templating
    - cwd option

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var cp = require("child_process"),
 	path = require("path"),
 	Duplexer = require("plexer"),
-	Stream = require("stream"),
-	gUtil = require("gulp-util");
+	gUtil = require("gulp-util"),
+	through = require("through2");
 
 var PLUGIN_NAME = "gulp-spawn";
 
-function gulpSpawn(options) {
+var gulpSpawn = function() {
 	"use strict";
 
-	var stream = new Stream.PassThrough({objectMode: true});
+	return gulpSpawn.stream.apply(this, arguments);
+};
+
+gulpSpawn.stream = function stream(options) {
+	"use strict";
 
 	// options.cmd required
 	if (!options.cmd) {
@@ -17,7 +21,7 @@ function gulpSpawn(options) {
 			"command (\"cmd\") argument required");
 	}
 
-	stream._transform = function (file, unused, cb) {
+	var stream = through.obj(function(file, unused, cb) {
 
 		if (file.isNull()) {
 			stream.push(file);
@@ -39,7 +43,7 @@ function gulpSpawn(options) {
 
 		// listen to stderr and emit errors if any
 		var errBuffer = new Buffer(0);
-		program.stderr.on("readable", function () {
+		program.stderr.on("readable", function() {
 			var chunk;
 			while (chunk = program.stderr.read()) {
 				errBuffer = Buffer.concat([
@@ -48,10 +52,10 @@ function gulpSpawn(options) {
 				], errBuffer.length + chunk.length);
 			}
 		});
-		program.stderr.on("end", function () {
+		program.stderr.on("end", function() {
 			if (errBuffer.length) {
 				stream.emit("error", new gUtil.PluginError(PLUGIN_NAME,
-				errBuffer.toString("utf-8")));
+					errBuffer.toString("utf-8")));
 			}
 		});
 
@@ -62,7 +66,7 @@ function gulpSpawn(options) {
 			var newBuffer = new Buffer(0);
 
 			// when program receives data add it to buffer
-			program.stdout.on("readable", function () {
+			program.stdout.on("readable", function() {
 				var chunk;
 				while (chunk = program.stdout.read()) {
 					newBuffer = Buffer.concat([
@@ -73,7 +77,7 @@ function gulpSpawn(options) {
 			});
 
 			// when program finishes call callback
-			program.stdout.on("end", function () {
+			program.stdout.on("end", function() {
 				file.contents = newBuffer;
 				stream.push(file);
 				cb();
@@ -81,22 +85,115 @@ function gulpSpawn(options) {
 
 			// "execute"
 			// write file buffer to program
-			program.stdin.write(file.contents, function () {
+			program.stdin.write(file.contents, function() {
 				program.stdin.end();
 			});
 
 		} else { // assume we have a stream.Readable
 
 			// stream away!
+			console.log("stream away!");
 			file.contents = file.contents
 				.pipe(new Duplexer(program.stdin, program.stdout));
 
 			stream.push(file);
 			cb();
 		}
-	};
+	});
 
 	return stream;
-}
+};
+
+gulpSpawn.once = function once(options) {
+	"use strict";
+
+	if (!options.cmd) {
+		throw new gUtil.PluginError(PLUGIN_NAME, "command (\"cmd\") argument required");
+	}
+
+	var stream = through.obj(function _transform(file, unused, cb) {
+		stream.push(file);
+		cb();
+	}, function _flush(cb) {
+		var spawnOpts = {
+			stdio: "inherit"
+		};
+
+		if (options.cwd) {
+			spawnOpts.cwd = options.cwd;
+		}
+
+		var program = cp.spawn(options.cmd, options.args, spawnOpts);
+		program.on("exit", function(code) {
+			if (code !== 0) {
+				stream.emit("error", new gUtil.PluginError(PLUGIN_NAME, "Command exited with code " + code +
+					"\nCommand: " + options.cmd + " " + options.args.map(JSON.stringify).join(" ")
+				));
+			}
+			cb();
+		});
+	});
+
+	return stream;
+};
+
+var processTemplates = function(args, file) {
+	"use strict";
+
+	var templateContext = {
+		file: file
+	};
+
+	if (typeof args === "string") {
+		return gUtil.template(args, templateContext);
+	} else if (Array.isArray(args)) {
+		return args.map(function(arg) {
+			return gUtil.template(arg, templateContext);
+		});
+	} else if (typeof args === "object") {
+		var r = {};
+		Object.keys(args).forEach(function(key) {
+			r[key] = gUtil.template(args[key], templateContext);
+		});
+
+		return r;
+	}
+};
+
+gulpSpawn.each = function each(options) {
+	"use strict";
+
+	if (!options.cmd) {
+		throw new gUtil.PluginError(PLUGIN_NAME, "command (\"cmd\") argument required");
+	}
+
+	var stream = through.obj(function _transform(file, unused, cb) {
+		stream.push(file);
+		if (file.isNull()) {
+			return cb();
+		}
+
+		var spawnOpts = {
+			stdio: "inherit"
+		};
+
+		if (options.cwd) {
+			spawnOpts.cwd = processTemplates(options.cwd, file);
+		}
+
+		var args = processTemplates(options.args, file);
+		var program = cp.spawn(options.cmd, args, spawnOpts);
+		program.on("exit", function(code) {
+			if (code !== 0) {
+				stream.emit("error", new gUtil.PluginError(PLUGIN_NAME, "Command exited with code " + code +
+					"\nCommand: " + options.cmd + " " + options.args.map(JSON.stringify).join(" ")
+				));
+			}
+			cb();
+		});
+	});
+
+	return stream;
+};
 
 module.exports = gulpSpawn;
